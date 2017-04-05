4e67684d621e41c674d50d03fce596b09730de63
xueli.xue <xueli.xue@dianping.com>
2017-04-05 16:02:24 +0800

    接口交互

--- xxl-api-admin/src/main/java/com/xxl/api/admin/controller/XxlApiTestController.java
+++ xxl-api-admin/src/main/java/com/xxl/api/admin/controller/XxlApiTestController.java
@@ -238,7 +238,7 @@ public class XxlApiTestController {
 				} else {
 					responseContent = "请求状态异常：" + response.getStatusLine().getStatusCode();
 					if (statusCode == 302) {
-						responseContent += "<br>Redirect地址：" + response.getHeaders("Location");
+						responseContent += "；Redirect地址：" + response.getHeaders("Location");
 					}
 
 				}
